299f1a9c42ca5cfd9ed9d6fadd2abf94bea32ea5
CGabrielOP <c.gabriel.oliveira.p@gmail.com>
2015-10-06 16:03:32 +0100

    o servidor ja tem uma thread para cada cliente

--- FundStarter/src/Client.java
+++ FundStarter/src/Client.java
@@ -9,84 +9,76 @@ import java.io.*;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author gabrieloliveira
  */
-public class Client extends Thread{
- 
-       public static void main(String args[]){
+public class Client extends Thread {
+
+    public static void main(String args[]) {
+
+        String ipServer1, ipServer2;
+        int port1, port2;
+        Socket conectionToServer;
+        String message = "Teste";
+        DataInputStream reciver;
+        DataOutputStream sender;
+
+        /*
+         se o utilizador iniciar o programa e não meter ip's e portas dos
+         servidores o programa entra neste if, imprime a mensagem de erro
+         e termina
            
-            String ipServer1,ipServer2;
-            int port1,port2;
-            Socket conectionToServer;
-            String message="Teste";
-            DataInputStream reciver;
-            DataOutputStream sender;
-           
+         PS: SE QUISEREM CORRER SÓ UM SERVIDOR PARA TERSTE METAM OS ULTIMOS
+         CAMPOS COM localhost e 0, ASSIM NÃO DÁ ERRO. :D
+         */
+        if (args.length != 4) {
+            System.out.println("java Client <IP1> <P1> <IP2> <P2>");
+            System.exit(0);
+        } else {
             /*
-            se o utilizador iniciar o programa e não meter ip's e portas dos
-            servidores o programa entra neste if, imprime a mensagem de erro
-            e termina
-           
-            PS: SE QUISEREM CORRER SÓ UM SERVIDOR PARA TERSTE METAM OS ULTIMOS
-            CAMPOS COM NULL, ASSIM NÃO DÁ ERRO. :D
-           */
-           if(args.length!=4){
-               System.out.println("java Client <IP1> <P1> <IP2> <P2>");
-               System.exit(0);
-           }
-           
-           else{
-               /*
-                Passa o argumentos para as variáveis para podermos fazer as
-                ligações mais à frente
-               */
-               
-               ipServer1=args[0];
-               port1=Integer.parseInt(args[1]);
-               ipServer2=args[2];
-               port2=Integer.parseInt(args[3]);
-               
-               try{
-                   
-                   /*
-                    cria ligação com o servidor que está com o IP->ipServer1 e
-                    Porto->port1
-                   */
-                   
-                   System.out.println("Fase 1->Conecção com servidor\n");
-                   conectionToServer=new Socket(ipServer1,port1);
-                   
-                   /*
-                    vai iniciar os streams de input e output para partilhar
-                    mensagens com o servidor
-                   */
-                   
-                   reciver=new DataInputStream(conectionToServer.getInputStream());
-                   sender=new DataOutputStream(conectionToServer.getOutputStream());
-                   
-                   
-                   /*
-                    vai mandar uma mensagem de teste ao servidor
-                   */
-                   
-                   System.out.println("Fase 2->Envio de mensagem ao servidro\n");
-                   sender.writeUTF(message);
-                   
-                   
-                   System.out.println("Fase 3->Receber mensagem do servidor\n");
-                   System.out.println(reciver.readUTF());
-                   
-               }catch(Exception e){
-                   /*Ainda não sei qual pode ser a excepção que dá aqui*/
-               }
-               
-               
-           }
-           
-           
-           
-       }
+             Passa o argumentos para as variáveis para podermos fazer as
+             ligações mais à frente
+             */
+
+            ipServer1 = args[0];
+            port1 = Integer.parseInt(args[1]);
+            ipServer2 = args[2];
+            port2 = Integer.parseInt(args[3]);
+
+            while (true) {
+                try {
+
+                    /*
+                     cria ligação com o servidor que está com o IP->ipServer1 e
+                     Porto->port1
+                     */
+                    System.out.println("Fase 1->Conecção com servidor\n");
+                    conectionToServer = new Socket(ipServer1, port1);
+
+                    /*
+                     vai iniciar os streams de input e output para partilhar
+                     mensagens com o servidor
+                     */
+                    reciver = new DataInputStream(conectionToServer.getInputStream());
+                    sender = new DataOutputStream(conectionToServer.getOutputStream());
+
+                    /*
+                     vai mandar uma mensagem de teste ao servidor
+                     */
+                    System.out.println("Fase 2->Envio de mensagem ao servidro\n");
+                    sender.writeUTF(message);
+
+                    System.out.println("Fase 3->Receber mensagem do servidor\n");
+                    System.out.println(reciver.readUTF());
+
+                } catch (Exception e) {
+                    /*Ainda não sei qual pode ser a excepção que dá aqui*/
+                }
+
+            }
+
+        }
+
+    }
 }
--- FundStarter/src/Server.java
+++ FundStarter/src/Server.java
@@ -2,62 +2,99 @@
 import java.net.*;
 import java.util.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author gabrieloliveira
  */
 public class Server {
-    
-    
-    public static void main(String args[]){
-        
-        try{
-            
+
+    public static void main(String args[]) {
+
+        try {
+
             /*
-                Cria um socket para ligação com clientes no porto indicado no
-                serverPort
-            */
-            int serverPort=6000;
-            ServerSocket conectionToClient=new ServerSocket(serverPort);
+             Cria um socket para ligação com clientes no porto indicado no
+             serverPort
+             */
+            int serverPort = 6000;
+            ServerSocket conectionToClient = new ServerSocket(serverPort);
             Socket cliente;
-            DataInputStream reciver;
-            DataOutputStream sender;
-            String message;
-            
+
             System.out.println("Servidor à eescuta no porto 6000");
-            
-            /*
-                Espera que um cliente se ligue
-            */
-            cliente=conectionToClient.accept();
-            
-            /*
-                cria canais de comunicação com os clientes
-            */
-            reciver=new DataInputStream(cliente.getInputStream());
-            sender=new DataOutputStream(cliente.getOutputStream());
-            
-             message=reciver.readUTF();
-             
-             if(message.equals("Teste")){
-                 message="Mesagem de teste recebida";
-             }
-             else{
-                 message="Error!!";
-             }
-             
-             sender.writeUTF(message);            
-            
-        }catch(Exception e){
-            /*falta aqui as excepções*/
+
+            while (true) {
+                /*
+                 Espera que um cliente se ligue
+                 */
+                cliente = conectionToClient.accept();
+                /*
+                 Por cada cliente que se liga, vai criar uma thread que fica
+                 encarregue de lidar com ele
+                 */
+                new NewClient(cliente);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Recebi esta mensagem de erro: " + e.getMessage());
         }
     }
-    
+
+}
+
+class NewClient implements Runnable {
+
+    Socket myClient;
+    DataInputStream reciver;
+    DataOutputStream sender;
+    Thread me;
+    String message;
+
+    NewClient(Socket cliente) {
+
+        try {
+            /*
+             Guarda o cliente que se ligou anteriormente ao servidor
+             */
+            myClient = cliente;
+
+            /*
+             cria canais de comunicação com os clientes
+             */
+            reciver = new DataInputStream(myClient.getInputStream());
+            sender = new DataOutputStream(myClient.getOutputStream());
+            
+            /*
+                vai iniciar a thread
+            */
+            me.start();
+
+        } catch (Exception e) {
+            System.out.println("Recebi esta mensagem de erro: " + e.getMessage());
+        }
+    }
+
+    public void run() {
+
+        try {
+            message = reciver.readUTF();
+
+            if (message.equals("Teste")) {
+                message = "Mesagem de teste recebida";
+            } else {
+                message = "Error!!";
+            }
+
+            sender.writeUTF(message);
+        } catch (Exception e) {
+            System.out.println("Recebi esta mensagem de erro: " + e.getMessage());
+        }
+    }
 }
