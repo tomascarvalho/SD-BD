701ee3b0d5f4493600dcea8ac9ca3053aa577413
dianaumbi <umbelino.diana@gmail.com>
2015-10-26 15:30:19 +0000

    Base de Dados

--- BD/ProjectoBDSD2/src/BD.java
+++ BD/ProjectoBDSD2/src/BD.java
@@ -20,19 +20,115 @@ public class BD {
 	ResultSet rs = null; 
 	String query;
 	
-	public static void main(String[] argv) {
-		//cria_user();
+	public static void main(String[] argv) throws SQLException {
+		
 		new BD();	
 	}
 	
-	public BD(){
+	public BD() throws SQLException{
 		DB();
-		//cria_user();
-		novoProjecto();
+		cria_user();
+//		novoProjecto();
+		//nova_mensagem();
+		//login();																	TO BE CONTINUED
 	}
 	
+	
+	//Rewards
+	public void rewards(int id, String tituloProj){
+		
+		System.out.println("Valor da recompensa:");
+		int r = sc.nextInt();
+		try{
+			query = "INSERT INTO recompensas (recompensa, id, titulo) VALUES (?, ?, ?)";
+            preparedstatement = connection.prepareStatement(query);
+            preparedstatement.setInt(1, r);
+            preparedstatement.setInt(2, id);
+            preparedstatement.setString(3, tituloProj);
+            preparedstatement.executeUpdate();
+            
+            
+            System.out.println("Rewards atribuidas!");
+		}
+		catch(SQLException e)
+        {
+			System.err.println("SQLException:"+e);
+        }
+		
+	}
+	
+	
+	//fazer login---------------------------------------------------NAO ESTA TERMINADO
+	public void login(){
+		String username, pass;
+		System.out.println("Login\nUsername:");
+		username = sc.next();
+		
+		System.out.println("Password:");
+		pass = sc.next();
+		
+		int iduser;
+		
+		try{
+			query = "SELECT id, username, pass FROM utilizador WHERE username = ? AND pass = ?";
+            preparedstatement = connection.prepareStatement(query);
+            preparedstatement.setString(1, username);
+            preparedstatement.setString(2, pass);
+    		rs = preparedstatement.executeQuery();
+            
+            
+            Statement request = connection.createStatement();
+            rs = request.executeQuery(query);
+            rs.next();
+    		iduser = rs.getInt("id");
+            
+    
+    		
+
+		}
+		catch(SQLException e)
+        {
+			System.err.println("SQLException:"+e);
+        }
+
+	}
+	
+	//envia recebe uma mensagem
+	public void nova_mensagem(){
+		
+		String pergunta, username;
+		String resposta;
+		
+		//só para inicializar as variáveis
+		System.out.println("O que deseja enviar, caro utilizador?");
+		pergunta = sc.nextLine();
+		
+		System.out.println("Quem sois?");
+		username = sc.nextLine();
+		
+		System.out.println("Responde lá então:");
+		resposta = sc.nextLine();
+		
+		
+		try{
+            query = "INSERT INTO mensagem (pergunta, username, resposta) VALUES (?,?,?)";
+            preparedstatement = connection.prepareStatement(query);
+            preparedstatement.setString(1, pergunta);
+            preparedstatement.setString(2, username);
+            preparedstatement.setString(3, resposta);
+            preparedstatement.executeUpdate();
+		}
+        catch(SQLException e)
+        {
+            System.err.println("SQLException:"+e);
+        }
+		
+	}
+	
+	
+	
 	//info do novo user
-	public void cria_user(){
+	public void cria_user() throws SQLException{
 		
 		String nome, apelido, username, password; 
 		System.out.println("Primeiro nome:");
@@ -46,40 +142,71 @@ public class BD {
 		
 		System.out.println("Password:");
 		password = sc.next();
-		User info = new User (nome, apelido, username, password);
 
+		try{
+            query = "INSERT INTO utilizador (nome, apelido, username, pass, saldo) VALUES (?,?,?,?,?)";
+            preparedstatement = connection.prepareStatement(query);
+            preparedstatement.setString(1,nome);
+            preparedstatement.setString(2,apelido);
+            preparedstatement.setString(3,username);
+            preparedstatement.setString(4, password);
+            preparedstatement.setInt(5, 100);
+            preparedstatement.executeUpdate();
+      
+            
+		}
+        catch(SQLException e)
+        {
+            System.err.println("SQLException:"+e);
+        }
+
+		System.out.println("Registo feito!\n");
 		
-	}
+		 System.out.println("Passar à criação de um novo projecto");
+		 try{
+	         query = "SELECT id FROM utilizador WHERE username = ? AND pass = ?";
+	
+	 		preparedstatement = connection.prepareStatement(query);
+	 		preparedstatement.setString(1, username);
+	 		preparedstatement.setString(2, password);
+	 		
+	 		rs = preparedstatement.executeQuery();
+	 		rs.next();
+	 		int getid = rs.getInt("id");
+	 		System.out.println(getid);
+	        novoProjecto(getid);
+		 }catch(SQLException e)
+	        {
+	            System.err.println("SQLException:"+e);
+	        }
+
+		 }
 	
 
-	public void novoProjecto(){
+	public void novoProjecto(int idUser) throws SQLException{
 		System.out.println("Criar um novo projecto:\n");
 		
-		System.out.println("Titulo:");
-		String tituloProjecto = sc.next();
+		System.out.println("Titulo: ");
+		String tituloProjecto = sc.nextLine();
 		
-		System.out.println("Descriçao:");
-		String descricao = sc.next();
-		
-/*		System.out.println("Data Limite");
-		Date dataLimite = ;*/
-		
-		System.out.println("Valor Pretendido");
+		System.out.println("Descriçao: ");
+		String descricao = sc.nextLine();
+			
+		System.out.println("Valor Pretendido: ");
 		int valorPretendido = sc.nextInt();
 		
-		System.out.println("Valor actual");
+		System.out.println("Valor actual: ");
 		int valorActual = sc.nextInt();
 				
-		try{
+		try{			
 			//inserção na tabela
-			query = "INSERT INTO projecto (titulo, descricao, valorPretendido, valorActual) VALUES(?,?,?,?)";
+			query = "INSERT INTO projecto (titulo, id_utilizador, descricao, valorPretendido, valorActual) VALUES(?,?,?,?,?)";
             preparedstatement = connection.prepareStatement(query);
-
             preparedstatement.setString(1, tituloProjecto);
-            //preparedstatement.setDate(2, dataLimite);
-            preparedstatement.setString(2, descricao);
-            preparedstatement.setInt(3, valorPretendido);
-            preparedstatement.setInt(4, valorActual);
+            preparedstatement.setInt(2, idUser);
+            preparedstatement.setString(3, descricao);
+            preparedstatement.setInt(4, valorPretendido);
+            preparedstatement.setInt(5, valorActual);
             preparedstatement.executeUpdate();
 		}
         catch(SQLException e)
@@ -88,6 +215,22 @@ public class BD {
         }
 
 		System.out.println("Dados guardados!\n");
+		
+		System.out.println("Deseja associar rewards?\n(1)-Sim	(2)-Não");
+		int aux = sc.nextInt();
+		
+		query = "SELECT id FROM projecto WHERE titulo = ?";
+		preparedstatement = connection.prepareStatement(query);
+		preparedstatement.setString(1, tituloProjecto);
+		rs = preparedstatement.executeQuery();
+		rs.next();
+		int getid = rs.getInt("id");
+		
+		if(aux == 1){
+
+			rewards(getid, tituloProjecto);
+		}
+		
 	}
 		    
 	
@@ -129,16 +272,7 @@ public class BD {
 		} else {
 			System.out.println("Failed to make connection!");
 		}
-		
-		
-		/*Statement stmt = null;
-	    String query = "INSERT INTO utilizador (id, nome, apelido, username, pass) VALUES("+100+",\'diana\', \'diana\', \'diana\', \'diana\');";
-		try {
-		    stmt = connection.createStatement();
-		    stmt.executeUpdate(query);
-	    } catch (SQLException e ) {
-	    	System.out.println("Failed query!"+e);
-	    }*/
+
 	}
 		
 	}
