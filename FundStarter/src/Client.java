4f19b2b8e05fe56e253122b89a08c5b4b4fb6dc3
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2015-10-29 15:57:10 +0000

    bug

--- FundStarter/src/Client.java
+++ FundStarter/src/Client.java
@@ -563,6 +563,7 @@ public class Client {
                 logResult = false;
                 
             }
+            menuConta();
            
         } else if (userPick.equals("3")) {
             listarProjectosActuais(0,0);
